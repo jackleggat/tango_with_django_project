c1b69d5c6e8d36d58f1455fff7bdf94a92b81f29
Jack Leggat <2314380l@student.gla.ac.uk>
2019-02-07 00:07:45 +0000

    Finished Chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -131,3 +131,10 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+LOGIN_URL='/rango/login/'
